269673f8757eb4cd72f0093acd143c5fa16fb83b
Daniel Santos <danielsantosdevweb@gmail.com>
2023-05-03 20:23:31 -0300

    add HashRoute

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import './index.css'
 import { GlobalStyle } from './GlobalStyle.jsx'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from './Theme'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme} >
-      <BrowserRouter>
+      <HashRouter>
         <GlobalStyle />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )
