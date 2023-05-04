d2ef7d3d692dc10fead1873ff695e02a0357fcd4
Daniel Santos <danielsantosdevweb@gmail.com>
2023-05-04 13:22:27 -0300

    mini atualizaçao

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,12 +5,12 @@ import { HomeStyle } from './HomeStyled'
 function Home() {
   return (
     <HomeStyle>
-      <h1>Bem Vindo ao meu Portifolio</h1>
+      <h1>Bem Vindo ao meu Portfolio</h1>
       <ImgStyle wid='200px' >
         <img src={Person} alt="" />
       </ImgStyle>
       <p>
-        Ola Desenvolvedores!! Sejam Bem Vindo ao meu <span>portifolio</span>... aqui voçe encontra mais sobre mim, as <span>tecnologias</span> que ultilizo e meus intereses na progamaçao, Tambem tem a aba com meus <span>projetos</span> e <span>contatos</span>.
+        Ola Desenvolvedores!! Sejam Bem Vindo ao meu <span>portfolio</span>... aqui voçe encontra mais sobre mim, as <span>tecnologias</span> que ultilizo e meus intereses na progamaçao, Tambem tem a aba com meus <span>projetos</span> e <span>contatos</span>.
       </p>
     </HomeStyle>
   )
